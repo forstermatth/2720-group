f9f97ba74782cc75504184d7ba25fb3035000075
Matt Forster <forster.matth@gmail.com>
2014-03-12 12:57:29 -0600

    CourseCont.it1 - pass: add course

--- include/CourseCont.h
+++ include/CourseCont.h
@@ -2,13 +2,16 @@
 #define __COURSECONT_H
 
 #include "Course.h"
+#include <list>
+using namespace std;
 
 class CourseCont {
 public:
 	void addCourse(Course _course);
 	Course first();
 private:
-
+	list<Course> courses;
+	list<Course>::iterator it;
 };
 
 #endif
--- src/CourseCont.cc
+++ src/CourseCont.cc
@@ -1,9 +1,10 @@
 #include "CourseCont.h"
 
 void CourseCont::addCourse(Course _course){
-
+	courses.push_front(_course);
 }
 
 Course CourseCont::first(){
-	
+	it = courses.begin();
+	return *it;
 }
