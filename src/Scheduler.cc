2325d39c559d64f59930cb1ec5665a08e7a56f60
justinwerre <werrejustin@gmail.com>
2014-04-12 16:32:59 -0600

    added some comments to the code

--- src/Scheduler.cc
+++ src/Scheduler.cc
@@ -12,19 +12,23 @@ CourseSched Scheduler::generateSchedule(CourseCont<Course>& courseList, Options 
 	bool reachEnd = false;
 
 	for(int i = 0; i < opts.getNumCourses() && !reachEnd;){
+		//if the current course is the last course in the container, exit the scheduler after trying to add this course.
 		reachEnd = courseList.get().equal(courseList.last());
 		Course add, comp;
 		bool labAdded = true;
 		add = comp = courseList.get(); 
 
+		//check to see if we can add the lab for this course, if there is a lab.
 		if(add.labs.size() > 0){
 			labAdded = false;
 			add.labs.begin();
 			while(!labAdded){
 				try{
+					//Need a temporary lab to check with padding.
 					Lab temp = add.labs.get();
 					temp.addPadding(opts.getBreakPadding());
 					schedule.findConflict(&temp);
+					//no time conflict, add the actual lab
 					schedule.addLab(add.labs.get());
 					labAdded = true;
 				}catch(const TimeConflict &expt){
@@ -37,6 +41,7 @@ CourseSched Scheduler::generateSchedule(CourseCont<Course>& courseList, Options 
 		}
 
 		try{
+			//if we can't add the lab, don't add the course
 			if(labAdded){
 				comp.addPadding(opts.getBreakPadding());
 				schedule.findConflict(&comp); //will throw and skip if conflict
