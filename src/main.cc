fdd8070430981fd524dbd48d1f05c18dbbc873cc
Huy Nguyen <huy.nguyen@uleth.ca>
2014-04-14 16:45:23 -0600

    Created production code (using testinput.xml)

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,27 @@
+#include "Output.h"
+#include "InputWrap.h"
+#include "Rater.h"
+#include "Scheduler.h"
+#include "CourseSched.h"
+#include "CourseCont.h"
+#include "Options.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
 
 int main(){
-	
-	
+  Output outp;
+  InputWrap input;
+  CourseCont _courseCont;
+  Options opt;
+  input.parse("../testfiles/testinput.xml");
+  opt = input.setOptions();
+  _courseCont = input.setCourses();
+  Rater _rater(opt, _courseCont);
+  _rater.rateCourses();
+  CourseSched sched;
+  Scheduler _scheduler;
+  sched = _scheduler.generateSchedule(_courseCont, opt);
+  outp.generate(opt, sched);
 }
