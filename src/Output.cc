03402ab7e19da8a90dcbb57355e084c80756dbea
forstermatth <forster.matth@gmail.com>
2014-04-14 18:38:07 -0600

    fix lab printing

--- src/Output.cc
+++ src/Output.cc
@@ -4,7 +4,7 @@ using namespace std;
 
 void Output::generate(Options opts, CourseSched sched){
   // Number of Options and and required classes
-	cout << "Options: " << endl
+	cout << endl << "Options: " << endl
 		 << "Number of classes:" << opts.getNumCourses() << endl
 		 << "Required Courses: " << endl;
 	// Gets the required courses
@@ -12,7 +12,7 @@ void Output::generate(Options opts, CourseSched sched){
 	 list<unsigned int>::iterator it;
 	 // iterates and outputs the required course IDs
 	 for(it = reqs.begin(); it != reqs.end(); it++){
-	 	cout << *it << endl;
+	 	cout << setw(15) << *it << endl;
 	 }
 	 // Display current time preferences and breaks
 	 cout << "Time Preference: " << opts.getTimePreference() << endl
@@ -20,7 +20,7 @@ void Output::generate(Options opts, CourseSched sched){
 	 	  << "Specific Break Start: " << opts.getBreakStart() << endl
 	 	  << "Specific Break End: "	  << opts.getBreakEnd() << endl;
 	 // Outputs the suggested schedule generated
-	cout << "Suggested schedule: " << endl;
+	cout << endl << "Suggested schedule: " << endl;
 	// starts are the beginning of container
 	Course c = sched.courses.begin();
 	do{
@@ -30,24 +30,22 @@ void Output::generate(Options opts, CourseSched sched){
  			<< setw(6) << c.getLoc() 		<< " " 
  			<< setfill('0') << setw(4) << c.getStartTime() << "-" << setfill('0') << setw(4) <<  c.getEndTime() << " " 
  			<< setfill(' ') << setw(3) << c.getDays() << endl;
-	  
-	  // if there are labs connected to the course, then they are displayed after
-		if(c.labs.size() > 0){
-			Lab l = c.labs.begin();
-			do{
-			  cout << setw(13) << l.getId() 		<< " " 
- 			 		<< setw(17) << l.getName() 	<< " " 
- 					<< setw(6) << l.getLoc() 		<< " " 
- 			 		<< setfill('0') << setw(4) << l.getStartTime() << "-" << setfill('0') << setw(4) << l.getEndTime() << " " 
- 			 		<< setfill(' ') << setw(3) << l.getDays() << endl;
-
-			  c.labs.next(); // move to next lab (if exists)
-			  l = c.labs.get(); // assign the lab
-			}while(!l.equal(c.labs.first()));
-		}
-
 		sched.courses.next(); // move to next course
 		c = sched.courses.get(); // assign the course
 	}while(!c.equal(sched.courses.first())); 
+	 // if there are labs scheduled
+	if(sched.labs.size() > 0){
+		Lab l = sched.labs.begin();
+		do{
+		  cout << setw(13) << l.getId() 		<< " " 
+			 		<< setw(17) << l.getName() 	<< " " 
+					<< setw(6) << l.getLoc() 		<< " " 
+			 		<< setfill('0') << setw(4) << l.getStartTime() << "-" << setfill('0') << setw(4) << l.getEndTime() << " " 
+			 		<< setfill(' ') << setw(3) << l.getDays() << endl;
+
+		  sched.labs.next(); // move to next lab (if exists)
+		  l = sched.labs.get(); // assign the lab
+		}while(!l.equal(sched.labs.first()));
+	}
 
 }
